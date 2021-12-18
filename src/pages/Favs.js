9ce3ef3a74394c27fcd8ec180b142c47db820711
sofiaperezm <sofi.prz.m@gmail.com>
2021-12-18 02:03:57 -0500

    added my faves page

--- src/pages/Favs.js
+++ src/pages/Favs.js
@@ -1,7 +1,39 @@
+import { useEffect, useState } from "react";
 import Main from "../layouts/Main";
+import Post from "../components/Post/Post";
+import { markAsFavorite, getFavoritePosts } from "../api/post";
 
 function FavsPage() {
-  return <Main currentPage="My faves">Favs page</Main>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const favoritePosts = getFavoritePosts();
+    setPosts(favoritePosts);
+  }, []);
+
+  function handleFavActions() {
+    const _posts = markAsFavorite(posts);
+    setPosts(_posts);
+  }
+
+  return (
+    <Main currentPage="My faves">
+      <div className="posts-container">
+        {posts.map((post) => (
+          <Post
+            key={post.id}
+            id={post.id}
+            author={post.author}
+            title={post.title}
+            url={post.url}
+            createdAt={post.createdAt}
+            isFav={post.isFav}
+            handleFavorite={handleFavActions}
+          />
+        ))}
+      </div>
+    </Main>
+  );
 }
 
 export default FavsPage;
